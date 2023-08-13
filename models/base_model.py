8e9aa243644e1e1bb58874def53beea2182486e6
mawusikpodo <mawusikpodo@yahoo.com>
2023-08-13 17:23:53 +0000

    added executables

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ Authors: RMK
 """
 import uuid
 from datetime import datetime
-import models
+from models import storage
 
 
 class BaseModel:
@@ -37,7 +37,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """str method for BaseModel Class
@@ -54,7 +54,7 @@ class BaseModel:
         current datetime
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__
